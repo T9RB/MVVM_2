cd5bf4d1be71f599f58f5c9f24a1c1ebd640e12e
Рыбалкин Никита <m9kish.play.tomsk@gmail.com>
2022-05-21 10:14:11 +0700

    Готово

--- WpfApp2/VM/VM_Profile_User.cs
+++ WpfApp2/VM/VM_Profile_User.cs
@@ -35,6 +35,7 @@ namespace MVVM_WPF
         public RelayCommand Report => _report ??
                                       (_report = new RelayCommand((x) =>
                                       {
+                                          ObservableCollection<WpfApp2.Task> tasks = new(Service.db.Tasks.Include(x => x.Status).Where(x => x.CreatorId == Service.user.Userid && x.AcceptorId == Service.user.Userid));
                                           if (MessageBox.Show("Если хотите отчет PDF, нажмите да, если EXCEL, то нет.",
                                                   "Save file",
                                                   MessageBoxButton.YesNo,
@@ -46,7 +47,6 @@ namespace MVVM_WPF
                                               PdfPage Page = _pdf.AddPage();
                                               XGraphics xgrap = XGraphics.FromPdfPage(Page);
                                               XFont FontPdf = new XFont("Calibri", 14);
-                                              ObservableCollection<WpfApp2.Task> tasks = new(Service.db.Tasks.Include(x => x.Status).Where(x => x.CreatorId == Service.user.Userid && x.AcceptorId == Service.user.Userid));
                                               xgrap.DrawString("Отчет по проделанной работе: ".ToString(), FontPdf, XBrushes.Black, new XRect(width, height, 0, 0));
                                               width = 0;
                                               height += 15;
@@ -83,18 +83,58 @@ namespace MVVM_WPF
                                               XLWorkbook wb = new();
                                               string filename = "Otchet".ToString();
                                               IXLWorksheet? ws = wb.Worksheets.Add("Otchet");
-
+                                              ws.Columns("A").AdjustToContents(40);
+                                              ws.Rows().AdjustToContents(20);
+                                              ws.Cell(1, 1).Value = Convert.ToString("Пользователь:");
+                                              ws.Cell(2, 1).Value = Convert.ToString(Service.user.FName);
+                                              ws.Cell(3, 1).Value = Convert.ToString(Service.user.SName);
+                                              ws.Cell(4, 1).Value = Convert.ToString(Service.user.LName);
+                                              ws.Cell(1, 2).Value = Convert.ToString("Имя");
+                                              ws.Cell(1, 3).Value = Convert.ToString("Дата");
+                                              ws.Cell(1, 3).Style.Border.BottomBorder = XLBorderStyleValues.Thick;
+                                              ws.Cell(1, 4).Value = Convert.ToString("Описание");
+                                              ws.Cell(1, 5).Value = Convert.ToString("Статус");
+                                              int xws = 2;
+                                              int yws = 2;
+                                              foreach (var item in tasks)
+                                              {
+                                                  ws.Cell(yws, xws).Value = item.NameTask;
+                                                  yws += 1;
+                                                  xws = 3;
+                                                  yws = 2;
+                                                  ws.Cell(yws, xws).Value = item.DatePub.ToString();
+                                                  yws += 1;
+                                                  xws = 4;
+                                                  yws = 2;
+                                                  ws.Cell(yws, xws).Value = item.DescriptionTask;
+                                                  yws += 1;
+                                                  xws = 5;
+                                                  yws = 2;
+                                                  ws.Cell(yws, xws).Value = item.Status.NameStatus;
+                                              }
+                                              ws.Column("A").AdjustToContents();
+                                              ws.Column("B").AdjustToContents();
+                                              ws.Column("C").AdjustToContents();
+                                              ws.Column("D").AdjustToContents();
                                               string path = null;
                                               VistaFolderBrowserDialog vfb = new VistaFolderBrowserDialog();
                                               vfb.ShowNewFolderButton = true;
-                                              
                                               if (vfb.ShowDialog() == true)
                                               {
                                                   path = vfb.SelectedPath;
                                               }
                                               if (path != null)
                                               {
-                                                  wb.SaveAs($"{path}//{filename}.xlsx");
+                                                  filename += ".xlsx" ;
+                                                  try
+                                                  {
+                                                      wb.SaveAs($"{path}//{filename}");
+                                                  }
+                                                  catch (Exception e)
+                                                  {
+                                                      MessageBox.Show("Не удается сохранить файл!");
+                                                      return;
+                                                  }
                                               }
                                               
                                           }
